df96d6d3b8363b79a01cfa13f86572152be1bc2d
tulsipratiba <sudhakartulsi@gmail.com>
2017-05-15 15:38:34 +0530

    update of comments

--- src/Addtwonumbers.java
+++ src/Addtwonumbers.java
@@ -4,8 +4,7 @@ public class Addtwonumbers {
 	public static void main(String[] args) 
 	{
 		
-		System.out.println(" Sum of 30 Add 100 is : " + Add(30, 100));
-		
+		System.out.println(" Sum of 30 Add 100 is : " + Add(30, 100));	
 
 	}
 
@@ -13,8 +12,8 @@ public class Addtwonumbers {
 public static int Add(int a, int b)
 {
     if(b == 0) return a;
-    int sum = a ^ b; 
-    int carry = (a & b) << 1;  // Addition of two numbers
+    int sum = a ^ b; 			//SUM of two integer is A XOR B
+    int carry = (a & b) << 1;  //CARRY of two integer is A AND B
     return Add(sum, carry);
 }
 
